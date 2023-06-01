d52639eaf05176250f6dfda924e735bd3065727b
Mateo Astelarra <mateo@astelarra.com>
2023-06-01 18:02:33 -0300

    Small changes

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -7,17 +7,19 @@ public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera  virtualCamera;
     [SerializeField] private KeyCode zoomKey = KeyCode.Mouse3;
+    [SerializeField] float zoomInValue = 25f;
+    [SerializeField] float zoomOutValue = 40f;
     
 
     private void Update() 
     {
         if (Input.GetKey(zoomKey))
         {
-            virtualCamera.m_Lens.FieldOfView = 30;
+            virtualCamera.m_Lens.FieldOfView = zoomInValue;
         }
         if (Input.GetKeyUp(zoomKey))
         {
-            virtualCamera.m_Lens.FieldOfView = 40;
+            virtualCamera.m_Lens.FieldOfView = zoomOutValue;
         }        
     }
 }
